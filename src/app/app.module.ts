346d634de42a212c555a79b5152e9d50eddeabc9
forsrc <forsrc@gmail.com>
2020-03-12 09:28:53 +0900

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import {
   MatToolbarModule,
 } from '@angular/material';
 
-import {AppRoutingModule} from './app-routing.module';
+import {AppRoutingModule} from './app.routing';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
