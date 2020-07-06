175fb683c85109d745d39384199aae72793c3f17
forsrc <forsrc@macos.local>
2020-07-06 14:51:57 +0900

    chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,20 +1,61 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ElementRef,
+  OnDestroy,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+  ViewEncapsulation
+} from '@angular/core';
+
+import { NgForm } from '@angular/forms';
+
+import { Router } from '@angular/router';
+
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewInit, OnDestroy {
+
+
+  @ViewChild('replyForm')
+  private _replyForm: NgForm;
+
+  @ViewChild('replyInput')
+  private _replyInput: ElementRef;
+
+
+
+  private _unsubscribeAll: Subject<any>;
+
   constructor(private router: Router) {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+
+  ngOnDestroy(): void {
+
   }
 
   onLogin() {
-    localStorage.setItem('isLoggedin', 'true');
-    this.router.navigate(['/chat']);
+    //localStorage.setItem('isLoggedin', 'true');
+    //this.router.navigate(['/chat']);
+  }
+
+  reply(event): void {
+    event.preventDefault();
+    alert(this._replyForm.form.value.message);
   }
 }
--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,14 +1,23 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatButtonModule, MatCheckboxModule, MatInputModule} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatInputModule,
+  MatRippleModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatTabsModule,
+  MatTooltipModule,
+} from '@angular/material';
 
-import {ChatRoutingModule} from './chat.routing.module';
-import {ChatComponent} from './chat.component';
-import {ChatUserListComponent} from './chat-user-list/chat-user-list.component';
-import {ChatMessageComponent} from './chat-message/chat-message.component';
+import { ChatRoutingModule } from './chat.routing.module';
+import { ChatComponent } from './chat.component';
+import { ChatUserListComponent } from './chat-user-list/chat-user-list.component';
+import { ChatMessageComponent } from './chat-message/chat-message.component';
 
 @NgModule({
   imports: [
@@ -17,6 +26,11 @@ import {ChatMessageComponent} from './chat-message/chat-message.component';
     MatInputModule,
     CommonModule,
     FormsModule,
+    MatRippleModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTabsModule,
+    MatTooltipModule,
     FlexLayoutModule,
     ChatRoutingModule
   ],
